5382f6c73d0bd92c7408381a2238601e8af90dd7
liuqiba <874740127@qq.com>
2022-06-01 11:58:24 +0800

    1 优化路径算法 2 修改算法为 泛型

--- arbitrage-CSharp/Tools/Algorithms.cs
+++ arbitrage-CSharp/Tools/Algorithms.cs
@@ -9,16 +9,16 @@ namespace Tools
 {
     public class Algorithms
     {
-        public List<int> DFS(Graph graph, int start)
+        public List<T> DFS<T>(Graph<T> graph, T start)
         {
-            var visited = new List<int>();
+            var visited = new List<T>();
 
             //如果没有这个节点直接返回
             if (!graph.AdjacencyList.ContainsKey(start))
             {
                 return visited;
             }
-            Stack<int> stack = new Stack<int>();
+            Stack<T> stack = new Stack<T>();
             //放入起始点
             stack.Push(start);
             while (stack.Count > 0)
@@ -51,38 +51,47 @@ namespace Tools
         /// <param name="graph"></param>
         /// <param name="start"></param>
         /// <returns></returns>
-        public Dictionary<int, List<List<int>>> DFSAllPaths(Graph graph, int start,int maxHops=10)
+        public Dictionary<int, List<List<T>>> DFSAllPaths<T>(Graph<T> graph, T start,int maxHops=10)
         {
-            var edgeListDic = new Dictionary<string, List<int>>();
-            GetAllEdgeList(graph, start, null, edgeListDic);
+            Dictionary<int, List<List<T>>> allRings = new Dictionary<int, List<List<T>>>();
+            var edgeListDic = new Dictionary<string, List<T>>();
+            //找到所有的 长边
+            GetAllEdgeList(graph, start, maxHops, null, edgeListDic);
 
-
-            var ringEdgeListDic = new Dictionary<string, List<int>>();
+            //过滤其中成环的和长度过载的
+            var ringEdgeListDic = new Dictionary<string, List<T>>();
             foreach (var item in edgeListDic)
             {
                 var v = item.Value;
-                if (v.Count>2)
+                //成环要求，至少要3个节点。。
+                //排除大于最大长度的环
+                if (v.Count>2 && v.Count< maxHops)
                 {
-
-
+                    
                     foreach (var neighbor in graph.AdjacencyList[v[v.Count-1]])
                     {
-                        if (neighbor == start)
+                        if (neighbor.Equals( start))
                         {
                             v.Add(start);
                             ringEdgeListDic.Add(item.Key, v);
+                            if (!allRings.TryGetValue(v.Count, out List<List<T>> paths))
+                            {
+                                paths = new List<List<T>>();
+                                allRings[v.Count] = paths;
+                            }
+                            paths.Add(v);
                         }
                     }
                 }
             }
-            return null;
+            return allRings;
         }
 
-        public void GetAllEdgeList(Graph graph, int start ,List<int> edgeList = null,  Dictionary<string, List<int>> edgeListDic=null )
+        public void GetAllEdgeList<T>(Graph<T> graph, T start, int maxHops,List<T> edgeList = null,  Dictionary<string, List<T>> edgeListDic=null )
         {
             if (edgeList==null)
             {
-                edgeList = new List<int>();
+                edgeList = new List<T>();
                 edgeList.Add(start);
             }
             //1 根据当前节点循环相邻节点，把没有的记录edgeListDic
@@ -97,30 +106,28 @@ namespace Tools
                 }
                 var newK = key.Substring(strs[0].Length+c);
 
-                string reKey = strs[0]+"_" + new string(newK.ToCharArray().Reverse().ToArray())  ;
+                string reKey = strs[0]+"_" + new string(newK.ToCharArray().Reverse().ToArray());
 
                 if (!edgeListDic.ContainsKey(key) && !edgeListDic.ContainsKey(reKey))
                 {
                     edgeListDic.Add(key, edgeList);
                 }
-                List<int> edgeListClone = new List<int>();
+                List<T> edgeListClone = new List<T>();
                 foreach (var item in edgeList)
                 {
                     edgeListClone.Add(item);
                 }
-                //有重复节点结束，没有重复节点继续寻找
-                if (!edgeListClone.Contains(neighbor))
+                //有重复节点结束，没有重复节点继续寻找,并且小于最大数量
+                if (!edgeListClone.Contains(neighbor) && edgeListClone.Count < maxHops)
                 {
                     edgeListClone.Add(neighbor);
-
-                    GetAllEdgeList(graph, neighbor, edgeListClone, edgeListDic);
+                    GetAllEdgeList(graph, neighbor, maxHops, edgeListClone, edgeListDic);
                 }
                 else
                 {
                     edgeListClone.Add(neighbor);
                 }
-                
-                
+
             }
         }
         public static void Test()
@@ -134,7 +141,7 @@ namespace Tools
             Tuple.Create(5, 6), Tuple.Create(8, 9), Tuple.Create(9, 10)
             };
 
-            var graph = new Graph(vertices, edges);
+            var graph = new Graph<int>(vertices, edges);
             var algorithms = new Algorithms();
 
             Logger.Debug(string.Join(", ", algorithms.DFS(graph, 1)));
@@ -154,11 +161,11 @@ namespace Tools
             Logger.Debug(sb.ToString());
         }
     }
-    public class Graph
+    public class Graph<T>
     {
         public Graph() { }
 
-        public Graph(IEnumerable<int> vertices, IEnumerable<Tuple<int, int>> edges)
+        public Graph(IEnumerable<T> vertices, IEnumerable<Tuple<T, T>> edges)
         {
             foreach (var vertex in vertices)
             {
@@ -171,20 +178,14 @@ namespace Tools
             }
         }
 
-        public Dictionary<int, HashSet<int>> AdjacencyList { get; } = new Dictionary<int, HashSet<int>>();
-        /// <summary>
-        /// 添加节点
-        /// </summary>
-        /// <param name="vertex"></param>
-        private void AddVertex(int vertex)
+        public Dictionary<T, HashSet<T>> AdjacencyList { get; } = new Dictionary<T, HashSet<T>>();
+
+        public void AddVertex(T vertex)
         {
-            AdjacencyList[vertex] = new HashSet<int>();
+            AdjacencyList[vertex] = new HashSet<T>();
         }
-        /// <summary>
-        /// 添加边,添加每个节点的相邻节点
-        /// </summary>
-        /// <param name="edge"></param>
-        private void AddEdge(Tuple<int, int> edge)
+
+        public void AddEdge(Tuple<T, T> edge)
         {
             if (AdjacencyList.ContainsKey(edge.Item1) && AdjacencyList.ContainsKey(edge.Item2))
             {
